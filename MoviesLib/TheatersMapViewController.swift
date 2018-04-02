8aee4f4113b6506452332e0e801c2daaf1d428fc
Usuário Convidado <Guest@pa06micro23.local>
2018-04-02 19:39:24 -0300

    Initial Commit

---
+++ MoviesLib/TheatersMapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TheatersMapViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 02/04/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class TheatersMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
